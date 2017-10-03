f40ec4c8b7f7a9a5fb70e2f630cd419dacbaacbc
CarlBoneri <carl.boneri@fas-advisory.com>
2017-10-03 15:32:00 -0400

    urls

---
+++ R/urls.R
@@ -0,0 +1,12 @@
+api_policy <- list(
+  facebook = "https://developers.facebook.com/policy/",
+  twitter = "https://developer.twitter.com/en/developer-terms/policy",
+  google_search = "https://developers.google.com/custom-search/terms",
+  reddit = "https://www.reddit.com/wiki/api-terms",
+  youtube = "https://developers.google.com/youtube/terms/api-services-terms-of-service",
+  google_plus = "https://developers.google.com/+/web/policies"
+)
+
+articles <- list(
+  linkedin_scrape = "https://www.theverge.com/2017/8/15/16148250/microsoft-linkedin-third-party-data-access-judge-ruling"
+)
